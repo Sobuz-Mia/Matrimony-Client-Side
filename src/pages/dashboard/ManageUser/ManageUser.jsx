9966cfe15afa89d876fccca2a9eb2e13e0d3743b
Sobuz <sobuz.info0298@gmail.com>
2023-12-02 13:19:01 +0600

    search user name field and get specifit user data

--- src/pages/dashboard/ManageUser/ManageUser.jsx
+++ src/pages/dashboard/ManageUser/ManageUser.jsx
@@ -10,14 +10,17 @@ import { TbPremiumRights } from "react-icons/tb";
 import Swal from "sweetalert2";
 import { FaEdit } from "react-icons/fa";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { useState } from "react";
+import { Grid } from "@mui/material";
 
 export default function ManageUser() {
   const axiosSecure = useAxiosSecure();
+  const [searchValue, setSearchValue] = useState("");
 
   const { data: users = [], refetch } = useQuery({
-    queryKey: ["users"],
+    queryKey: ["users",searchValue],
     queryFn: async () => {
-      const res = await axiosSecure.get("/users");
+      const res = await axiosSecure.get(`/users?search=${searchValue}`);
       return res?.data;
     },
   });
@@ -72,65 +75,88 @@ export default function ManageUser() {
     });
   };
   return (
-    <TableContainer component={Paper} sx={{ mt: 2 }}>
-      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-        <TableHead>
-          <TableRow>
-            <TableCell style={{ fontSize: "18px", fontWeight: "bold" }}>
-              SL
-            </TableCell>
-            <TableCell style={{ fontSize: "18px", fontWeight: "bold" }}>
-              Name
-            </TableCell>
-            <TableCell
-              align="center"
-              style={{ fontSize: "18px", fontWeight: "bold" }}
-            >
-              Email
-            </TableCell>
-            <TableCell
-              align="center"
-              style={{ fontSize: "18px", fontWeight: "bold" }}
-            >
-              Make Admin
-            </TableCell>
+    <>
+      <Grid sx={{display:'flex',flexDirection:'column',gap:'5px',mt:'5px'}}>
+        <label style={{ color: "#E33183" ,fontSize:'20px'}}>Search user</label>
+        <input
+          onChange={(e) => setSearchValue(e.target.value)}
+          type="text"
+          value={searchValue}
+          style={{
+            padding: "10px",
+            borderRadius: "5px",
+            border: "1px solid",
+            width: "300px",
+          }}
+          placeholder="Search user name"
+          required
+        />
+      </Grid>
 
-            <TableCell
-              align="center"
-              style={{ fontSize: "18px", fontWeight: "bold" }}
-            >
-              Make Premium
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {users.map((item, index) => (
-            <TableRow key={item._id}>
-              <TableCell component="th" scope="row">
-                {index + 1}
+      <TableContainer component={Paper} sx={{ mt: 2 }}>
+        <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell style={{ fontSize: "18px", fontWeight: "bold" }}>
+                SL
               </TableCell>
-              <TableCell component="th" scope="row">
-                {item?.userName}
-              </TableCell>
-              <TableCell align="center">{item?.email}</TableCell>
-              <TableCell
-                onClick={() => handleMakeAdmin(item?._id)}
-                align="center"
-                sx={{ fontSize: "20px" }}
-              >
-                {item?.role == "Admin" ?"Admin" :<FaEdit />}
+              <TableCell style={{ fontSize: "18px", fontWeight: "bold" }}>
+                Name
               </TableCell>
               <TableCell
-                onClick={() => handlePremimuUser(item?._id)}
                 align="center"
-                style={{ color: "red", fontSize: "20px" }}
+                style={{ fontSize: "18px", fontWeight: "bold" }}
               >
-                {item?.userStatus=="premium"?"Premium" :<TbPremiumRights />}
+                Email
+              </TableCell>
+              <TableCell
+                align="center"
+                style={{ fontSize: "18px", fontWeight: "bold" }}
+              >
+                Make Admin
+              </TableCell>
+
+              <TableCell
+                align="center"
+                style={{ fontSize: "18px", fontWeight: "bold" }}
+              >
+                Make Premium
               </TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {users.map((item, index) => (
+              <TableRow key={item._id}>
+                <TableCell component="th" scope="row">
+                  {index + 1}
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {item?.userName}
+                </TableCell>
+                <TableCell align="center">{item?.email}</TableCell>
+                <TableCell
+                  onClick={() => handleMakeAdmin(item?._id)}
+                  align="center"
+                  sx={{ fontSize: "20px" }}
+                >
+                  {item?.role == "Admin" ? "Admin" : <FaEdit />}
+                </TableCell>
+                <TableCell
+                  onClick={() => handlePremimuUser(item?._id)}
+                  align="center"
+                  style={{ color: "red", fontSize: "20px" }}
+                >
+                  {item?.userStatus == "premium" ? (
+                    "Premium"
+                  ) : (
+                    <TbPremiumRights />
+                  )}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
